e133eb985d347c5799d58943f14d9e85b12ca18a
Fernando Paladini <fnpaladini@gmail.com>
2015-06-04 21:06:20 -0300

    First version of variables working!

--- src/Run.cpp
+++ src/Run.cpp
@@ -11,14 +11,19 @@
 #include "CommandInterpreter.hpp"
 #include "MakeFile.hpp"
 #include "Target.hpp"
+#include "Variable.hpp"
 
 
 int main(int argc, char* argv[]) {
 	
 	// Creating a makefile
-	CommandInterpreter interpreter(argc, argv);
+	// CommandInterpreter interpreter(argc, argv);
 	// MakeFile mk = interpreter.parse();
-	interpreter.parse();
+	// interpreter.parse();
+
+	
+	// MakeFile _mk("makefile", false);
+	// _mk
 	// Test for the makefile parsing.
 	//MakeFile _mk("makefile", false);
 	//std::cout << "Nome do target no Run.cpp: " <<  _mk.get_targets().at(0).get_title()<< std::endl;
@@ -28,23 +33,31 @@ int main(int argc, char* argv[]) {
 	//std::cout << "\n=========================\nImprimindo todo o Makefile interpretado: \n" << std::endl;
 	//std::cout << _mk.to_string() << std::endl;
 
-	/* Usage example:
+	 // Usage example:
+	Variable v1("key1", "value1");
+	Variable v2("key2", "value2");
+	Variable v3("key3", "value3");
 	Flag flag1("-output");
 	Flag flag2("-force");
-	
-	Compiler gpp("g++");
-	
 	File file1("test1.cpp");
 	File file2("test2.cpp");
 	
-	Command line1(gpp);
+	Command line1("g++");
 	line1.add_file(file1);
 	line1.add_file(file2);
 	line1.add_flag(flag1);
 	line1.add_flag(flag2);
+
+	Target t1("myTarget");
+	t1.add_command(line1);
 	
-	FileManager myAwesomeMakefile("makefile");
-	myAwesomeMakefile.write(line1.to_command()); */
+	MakeFile myAwesomeMakefile("makefile");
+	myAwesomeMakefile.add_variable(v1);
+	myAwesomeMakefile.add_variable(v2);
+	myAwesomeMakefile.add_variable(v3);
+	myAwesomeMakefile.add_target(t1);
+
+	myAwesomeMakefile.save(); 
 	
 		
 };
